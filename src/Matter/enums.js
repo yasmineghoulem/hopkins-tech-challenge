68469bd35b2abb8040116b21a1c1aad34f62f628
Sergei Reutov <basic.mehmat@gmail.com>
2023-08-24 17:41:48 +0200

    AreasOfLaw enum

---
+++ src/Matter/enums.js
@@ -0,0 +1,18 @@
+export const AREAS_OF_LAW = {
+  ALL: {
+    label: 'Alle',
+    value: 'all',
+  },
+  LABOR: {
+    label: 'Arbeitsrecht',
+    value: 'Arbeitsrecht',
+  },
+  TAX: {
+    label: 'Steuerrecht',
+    value: 'Steuerrecht',
+  },
+  SOCIAL: {
+    label: 'Sozialrecht',
+    value: 'Sozialrecht',
+  },
+};
